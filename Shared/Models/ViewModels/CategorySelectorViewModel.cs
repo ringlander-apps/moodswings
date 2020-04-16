bf075ea3cf06942830df9e20ae541b6ed5bf651b
ringlander-apps <dev.ringlander@gmail.com>
2020-04-16 15:06:38 +0200

    Creating view model for Category Selection Part

---
+++ Shared/Models/ViewModels/CategorySelectorViewModel.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel;
+using System.Collections.Generic;
+using MoodSwings.Shared.Models;
+
+namespace MoodSwings.Shared.Models.ViewModels
+{
+    public class SelectCategoryViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private List<string> _categories = new List<string> { "Artist", "Playlist" };
+
+        public List<string> AvailableCategories
+        {
+            get => _categories;
+            set { _categories = value; }
+        }
+        private string _selectedCategory = "Artist";
+        public string SelectedCategory
+        {
+            get => _selectedCategory;
+            set { _selectedCategory = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedCategory))); }
+        }
+
+        public void SetCategory(string category)
+        {
+            if (_selectedCategory != category)
+            {
+                SelectedCategory = category;
+
+            }
+        }
+
+    }
+
+}
