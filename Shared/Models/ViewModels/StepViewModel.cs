9aaf60234467c2ebeca0e0ec94d99c65d0257c15
ringlander-apps <dev.ringlander@gmail.com>
2020-04-16 15:07:21 +0200

    Created ViewModel for the GuideSteps part

---
+++ Shared/Models/ViewModels/StepViewModel.cs
@@ -0,0 +1,12 @@
+namespace MoodSwings.Shared.Models.ViewModels
+{
+    public class StepModel
+    {
+        public int Id { get; set; }
+        public bool IsSelected { get; set; }
+
+        public string Caption { get; set; }
+
+    }
+
+}
